cbbe897f67e3f6234c7962136b0e330f6e250dc7
centinela1024tb <centinela1024tb@gmail.com>
2022-03-01 23:50:19 -0600

    [ADD] Se agrega configuración basica del server con express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola");
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hola mundo con express");
+});
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server run on port ${port}`);
+});
