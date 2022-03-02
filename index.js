a51a4bbb0a051429b41fd05dc57c6e7b5b38ce2a
centinela1024tb <centinela1024tb@gmail.com>
2022-03-01 23:58:08 -0600

    [ADD] Se agrega script dev para ejecutar el index.js con nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Hola mundo con express");
+  res.send("Hola mundo con express!!");
 });
 
 const port = 4000;
